c1a297e62353114fcffb821dc63aa8f7c0095939
Sumaiya Islam Mim <2110903@iub.edu.bd>
2026-04-24 17:30:39 +0600

    Add product to cart functionality in ProductDetailsPage
    Implemented addToCart method to add product to Firestore cart.

--- shopping_app/lib/pages/product_details.dart
+++ shopping_app/lib/pages/product_details.dart
@@ -1,1 +1,39 @@
+import 'package:flutter/material.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
 
+class ProductDetailsPage extends StatelessWidget {
+  final dynamic product;
+
+  const ProductDetailsPage({super.key, required this.product});
+
+  void addToCart() {
+    FirebaseFirestore.instance.collection('cart').add({
+      'name': product['name'],
+      'price': product['price'],
+      'imageUrl': product['imageUrl'],
+      'quantity': 1,
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: Text(product['name'])),
+      body: Column(
+        children: [
+          Image.network(product['imageUrl'], height: 250),
+          Text(product['name'], style: const TextStyle(fontSize: 22)),
+          Text("৳ ${product['price']}"),
+          Padding(
+            padding: const EdgeInsets.all(10),
+            child: Text(product['description'] ?? ""),
+          ),
+          ElevatedButton(
+            onPressed: addToCart,
+            child: const Text("Add to Cart"),
+          ),
+        ],
+      ),
+    );
+  }
+}
